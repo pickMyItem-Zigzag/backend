f24df6764a53cf3f51a751ee340168d82cceafad
woonsik <dnstlr2933@naver.com>
2022-09-28 22:47:47 +0900

    feat: init project

---
+++ src/main/kotlin/kakaostyle/pickMyItem/PickMyItemApplication.kt
@@ -0,0 +1,11 @@
+package kakaostyle.pickMyItem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PickMyItemApplication
+
+fun main(args: Array<String>) {
+	runApplication<PickMyItemApplication>(*args)
+}
