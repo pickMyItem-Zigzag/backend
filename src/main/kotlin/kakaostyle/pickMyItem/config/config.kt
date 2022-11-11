44dd7e9f239d3d7fc590ede7e9d20856fbca3d36
groom <groom.siki@kakaostyle.com>
2022-11-11 14:08:14 +0900

    feat: cors 설정 추가

--- src/main/kotlin/kakaostyle/pickMyItem/config/config.kt
+++ src/main/kotlin/kakaostyle/pickMyItem/config/config.kt
@@ -1,21 +1,34 @@
 package kakaostyle.pickMyItem.config
 
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 class config
 
-@Bean
-fun corsConfigurer(): WebMvcConfigurer {
-    return object : WebMvcConfigurer {
-        override fun addCorsMappings(registry: CorsRegistry) {
-            registry.addMapping("/graphql/**")
-                .allowedOrigins(CorsConfiguration.ALL)
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowedMethods(CorsConfiguration.ALL)
-        }
+@Configuration
+class corsConfigurer : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(CorsConfiguration.ALL)
+            .allowedHeaders(CorsConfiguration.ALL)
+            .allowedMethods(CorsConfiguration.ALL)
     }
+}
+
+@Bean
+fun corsFilter(): CorsFilter {
+    val source = UrlBasedCorsConfigurationSource()
+    val config = CorsConfiguration()
+    config.allowCredentials = true
+    config.addAllowedOrigin("*")
+    config.addAllowedHeader("*")
+    config.addAllowedMethod("*")
+    source.registerCorsConfiguration("/graphql/**", config)
+    return CorsFilter(source)
 }
