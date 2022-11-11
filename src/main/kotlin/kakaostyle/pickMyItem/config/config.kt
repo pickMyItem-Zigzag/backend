14c957003c4d0a19dbb077426f7ebea6076836cd
groom <groom.siki@kakaostyle.com>
2022-11-11 14:09:57 +0900

    feat: cors 설정 추가

--- src/main/kotlin/kakaostyle/pickMyItem/config/config.kt
+++ src/main/kotlin/kakaostyle/pickMyItem/config/config.kt
@@ -1,12 +1,8 @@
 package kakaostyle.pickMyItem.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 class config
@@ -19,16 +15,4 @@ class corsConfigurer : WebMvcConfigurer {
             .allowedHeaders(CorsConfiguration.ALL)
             .allowedMethods(CorsConfiguration.ALL)
     }
-}
-
-@Bean
-fun corsFilter(): CorsFilter {
-    val source = UrlBasedCorsConfigurationSource()
-    val config = CorsConfiguration()
-    config.allowCredentials = true
-    config.addAllowedOrigin("*")
-    config.addAllowedHeader("*")
-    config.addAllowedMethod("*")
-    source.registerCorsConfiguration("/graphql/**", config)
-    return CorsFilter(source)
 }
