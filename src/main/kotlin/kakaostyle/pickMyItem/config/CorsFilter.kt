f008cfd1ec5fec607b0c6e59a5af8d28e20501bd
groom <groom.siki@kakaostyle.com>
2022-11-22 09:58:48 +0900

    feat: cors allowedOrigins 추가

--- src/main/kotlin/kakaostyle/pickMyItem/config/CorsFilter.kt
+++ src/main/kotlin/kakaostyle/pickMyItem/config/CorsFilter.kt
@@ -1,18 +1,25 @@
 package kakaostyle.pickMyItem.config
 
-//@Configuration
-//class CorsConfig {
-//
-//    @Bean
-//    fun corsFilter(): CorsFilter {
-//        val source = UrlBasedCorsConfigurationSource()
-//        val config = CorsConfiguration()
-//        config.allowCredentials = true
-//        config.addAllowedOrigin("http://localhost")
-//        config.addAllowedHeader("*")
-//        config.addAllowedMethod("*")
-//        config.allowedOriginPatterns = listOf("*")
-//        source.registerCorsConfiguration("/**", config)
-//        return CorsFilter(source)
-//    }
-//}
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("http://localhost")
+        config.addAllowedOrigin("http://pick-item-client.s3-website.ap-northeast-2.amazonaws.com")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        config.allowedOriginPatterns = listOf("*")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
